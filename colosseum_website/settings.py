655a654399f184d66d22e521911ba28c50fe5471
h3nnn4n <uber.renan@gmail.com>
2021-11-14 22:52:14 -0300

    use django as backend

--- colosseum_website/settings.py
+++ colosseum_website/settings.py
@@ -75,7 +75,14 @@ WSGI_APPLICATION = "colosseum_website.wsgi.application"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3"}
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "colosseum",
+        "USER": "colosseum",
+        "PASSWORD": "potato",
+        "HOST": "localhost",
+        "PORT": "5432",
+    }
 }
 
 
