33121e590f5624a317b0526b731616a85c8c4213
h3nnn4n <uber.renan@gmail.com>
2021-11-15 07:22:15 -0300

    Remove app/ prefix

--- colosseum_website/urls.py
+++ colosseum_website/urls.py
@@ -2,4 +2,4 @@ from django.contrib import admin
 from django.urls import include, path
 
 
-urlpatterns = [path("app/", include("app.urls")), path("admin/", admin.site.urls)]
+urlpatterns = [path("", include("app.urls")), path("admin/", admin.site.urls)]
