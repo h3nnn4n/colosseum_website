da2807a961f88a7692971402e0d54854450e7cca
h3nnn4n <uber.renan@gmail.com>
2021-11-14 22:35:38 -0300

    add djongo

--- colosseum_website/settings.py
+++ colosseum_website/settings.py
@@ -75,7 +75,18 @@ WSGI_APPLICATION = "colosseum_website.wsgi.application"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3"}
+    "default": {
+        "ENGINE": "djongo",
+        "NAME": "colosseum",
+        "CLIENT": {
+            "host": "localhost",
+            "port": 27017,
+            # "username": "",
+            # "password": "",
+            # "authSource": "",
+            # "ssl": True,
+        },
+    }
 }
 
 
