785c0892456a6756cb3c04337fd6d5e62366d33b
h3nnn4n <uber.renan@gmail.com>
2021-11-16 10:07:23 -0300

    Add admin to models

--- app/admin.py
+++ app/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 
+from app import models
 
-# Register your models here.
+
+admin.site.register(models.Agent)
+admin.site.register(models.Game)
+admin.site.register(models.Match)
+admin.site.register(models.Tournament)
