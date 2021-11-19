5afb391728281f4122c7f95f79dc01dc614c3b2d
h3nnn4n <uber.renan@gmail.com>
2021-11-19 17:56:08 -0300

    Remove double log entries

--- colosseum_website/settings/base.py
+++ colosseum_website/settings/base.py
@@ -138,10 +138,7 @@ LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
     "handlers": {"console": {"class": "logging.StreamHandler"}},
-    "loggers": {
-        "": {"handlers": ["console"], "level": "INFO"},
-        "django": {"handlers": ["console"], "level": "INFO"},
-    },
+    "loggers": {"": {"handlers": ["console"], "level": "INFO"}},
 }
 
 
