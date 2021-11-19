b3443d60d1a58c0944ee73382c5d1ac230b98465
h3nnn4n <uber.renan@gmail.com>
2021-11-19 10:48:01 -0300

    Get bucket name and region from env

--- colosseum_website/settings/base.py
+++ colosseum_website/settings/base.py
@@ -148,8 +148,8 @@ LOGGING = {
 SHELL_PLUS = "ipython"
 
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-AWS_STORAGE_BUCKET_NAME = "colosseum-agents-dev"
-AWS_REGION = "eu-east-1"
+AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
+AWS_REGION = os.environ.get("AWS_REGION", "eu-east-1")
 
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
