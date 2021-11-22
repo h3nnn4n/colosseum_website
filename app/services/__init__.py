d9206d2e4b661bc871b3dcd075dad224b30750b7
h3nnn4n <uber.renan@gmail.com>
2021-11-22 14:14:53 -0300

    Add util to purge all tournaments

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -4,3 +4,8 @@ from .. import models
 def purge_all_played_games():
     models.Agent.objects.all().update(wins=0, loses=0, draws=0, elo=1500, score=0)
     models.Match.objects.all().delete()
+
+
+def purge_all_tournaments():
+    models.Tournament.objects.all().delete()
+    models.Match.objects.all().delete()
