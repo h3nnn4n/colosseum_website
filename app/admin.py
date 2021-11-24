1eb205dffbea5dc096190856f18ca935263243df
h3nnn4n <uber.renan@gmail.com>
2021-11-24 12:18:01 -0300

    Improve admin list page

--- app/admin.py
+++ app/admin.py
@@ -3,7 +3,21 @@ from django.contrib import admin
 from app import models
 
 
-admin.site.register(models.Agent)
-admin.site.register(models.Game)
-admin.site.register(models.Match)
-admin.site.register(models.Tournament)
+@admin.register(models.Agent)
+class AgentAdmin(admin.ModelAdmin):
+    list_display = ("name", "id", "owner", "wins", "loses", "draws", "score", "elo")
+
+
+@admin.register(models.Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ("name", "id")
+
+
+@admin.register(models.Match)
+class MatchAdmin(admin.ModelAdmin):
+    list_display = ("id", "player1", "player2", "ran", "ran_at")
+
+
+@admin.register(models.Tournament)
+class TournamentAdmin(admin.ModelAdmin):
+    list_display = ("name", "id", "mode", "start_date", "end_date")
