56cd1ddd2efe90e1284e0eb1031dde175f8abea9
h3nnn4n <uber.renan@gmail.com>
2021-11-24 17:57:42 -0300

    Add automated tournaments for all types

--- app/services/automated_tournaments.py
+++ app/services/automated_tournaments.py
@@ -4,13 +4,20 @@ from .. import models, serializers
 
 
 def create_automated_tournaments():
-    create_daily_tournament()
+    _create_automated_tournament("Automated Daily Tournament #{}", "TIMED")
+    _create_automated_tournament("Automated Round Robin Tournament #{}", "ROUND_ROBIN")
+    _create_automated_tournament(
+        "Automated Double Round Robin Tournament #{}", "DOUBLE_ROUND_ROBIN"
+    )
+    _create_automated_tournament(
+        "Automated Triple Round Robin Tournament #{}", "TRIPLE_ROUND_ROBIN"
+    )
 
 
-def create_daily_tournament():
+def _create_automated_tournament(name, mode):
     """
-    Creates a timed tournament that lasts for one day. If such a tournament
-    already exists it does nothing, otherwise it creates a new one.
+    Creates an automated tournament. If such a tournament already exists it
+    does nothing, otherwise it creates a new one.
     """
     # FIXME: This should not be hardcoded
     game = models.Game.objects.first().id
@@ -19,24 +26,24 @@ def create_daily_tournament():
     start_date = now.replace(hour=0, minute=0, second=0, microsecond=0)
     end_date = now.replace(hour=23, minute=59, second=59, microsecond=999)
 
-    last_automated_daily_tournament = (
-        models.Tournament.objects.filter(mode="TIMED", is_automated=True)
+    last_automated_tournament = (
+        models.Tournament.objects.filter(mode=mode, is_automated=True)
         .order_by("-automated_number")
         .first()
     )
 
-    if last_automated_daily_tournament:
-        next_number = last_automated_daily_tournament.automated_number + 1
+    if last_automated_tournament:
+        next_number = last_automated_tournament.automated_number + 1
     else:
         next_number = 1
 
-    if last_automated_daily_tournament and last_automated_daily_tournament.is_active:
-        return {"status": "active daily tournament already exists"}
+    if last_automated_tournament and last_automated_tournament.is_active:
+        return {"status": "active tournament already exists"}
 
-    name = f"Automated Daily Tournament #{next_number}"
+    name = name.format(next_number)
 
     data = {
-        "mode": "TIMED",
+        "mode": mode,
         "start_date": start_date,
         "end_date": end_date,
         "game": game,
