b03ab0e419a8444e6be724166ff57bb4919ad100
h3nnn4n <uber.renan@gmail.com>
2021-11-25 07:46:13 -0300

    Add matches per minute over a day plot endpoint

--- app/plots.py
+++ app/plots.py
@@ -5,7 +5,7 @@ from datetime import timedelta
 import matplotlib
 import numpy as np
 from django.db.models import Count
-from django.db.models.functions import TruncHour
+from django.db.models.functions import TruncMinute
 from django.http import HttpResponse
 from django.utils import timezone
 
@@ -14,30 +14,36 @@ from . import models
 
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt  # noqa
+import seaborn as sns  # noqa
 
 
 def matches_per_day():
+    sns.set_theme(style="whitegrid")
+    sns.set_color_codes("pastel")
+
     data = (
         models.Match.objects.filter(
             ran=True, ran_at__gte=timezone.now() - timedelta(days=1)
         )
-        .annotate(date=TruncHour("ran_at"))
+        .annotate(date=TruncMinute("ran_at"))
         .values("date")
         .annotate(count=Count("date"))
         .values("date", "count")
         .order_by("date")
     )
 
-    fig, ax = plt.subplots(figsize=(10, 8))
-    ax.plot([d["date"] for d in data], [d["count"] for d in data])
+    with sns.axes_style("whitegrid"):
+        figure, ax = plt.subplots(figsize=(12, 8))
+        sns.lineplot(x=[d["date"] for d in data], y=[d["count"] for d in data])
 
-    # fig.autofmt_xdate()
+    sns.despine(top=True, right=True, left=True, bottom=True)
+
     ax.set_title("Matches in the last 24 hours")
-    ax.set_ylabel("Count")
-    ax.set_xlabel("Date")
-    ax.grid(linestyle="--", linewidth=0.5, color=".25", zorder=-10)
+    ax.set_ylabel("Matches per minute")
 
-    return _make_response(fig)
+    figure.tight_layout()
+
+    return _make_response(figure)
 
 
 def _make_response(fig):
