b73a96099c98ef64b3ff30876aef77a1dae28fa5
h3nnn4n <uber.renan@gmail.com>
2021-12-26 18:04:21 -0300

    Add some filters and search to django admin

--- app/admin.py
+++ app/admin.py
@@ -6,6 +6,7 @@ from app import models
 @admin.register(models.Agent)
 class AgentAdmin(admin.ModelAdmin):
     list_display = ("name", "id", "owner", "wins", "loses", "draws", "score", "elo")
+    search_fields = ("name", "id", "owner")
 
 
 @admin.register(models.Season)
@@ -21,8 +22,11 @@ class GameAdmin(admin.ModelAdmin):
 @admin.register(models.Match)
 class MatchAdmin(admin.ModelAdmin):
     list_display = ("id", "player1", "player2", "ran", "ran_at")
+    list_filter = ("ran",)
 
 
 @admin.register(models.Tournament)
 class TournamentAdmin(admin.ModelAdmin):
     list_display = ("name", "id", "mode", "start_date", "end_date")
+    list_filter = ("mode",)
+    search_fields = ("name", "id")
