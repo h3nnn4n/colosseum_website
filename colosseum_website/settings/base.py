8673bd92a96d3913e457334177e67419b9062b75
h3nnn4n <uber.renan@gmail.com>
2021-12-26 20:05:24 -0300

    Add drf pagination

--- colosseum_website/settings/base.py
+++ colosseum_website/settings/base.py
@@ -145,6 +145,8 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.TokenAuthentication"
     ],
     "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAdminUser"],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "PAGE_SIZE": 100,
 }
 
 LOGGING = {
