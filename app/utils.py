27e99161adf3c3cbb71c0b2013b06d1b0f68c555
h3nnn4n <uber.renan@gmail.com>
2022-05-01 15:09:19 -0300

    Blackfy

--- app/utils.py
+++ app/utils.py
@@ -12,7 +12,7 @@ def replay_filepath(match, filename):
     return f"replays/{match.tournament.id}/{match.id}/{filename}"
 
 
-def hash_file(file, blocksize=2 ** 20):
+def hash_file(file, blocksize=2**20):
     m = hashlib.sha1()
     while True:
         buf = file.read(blocksize)
